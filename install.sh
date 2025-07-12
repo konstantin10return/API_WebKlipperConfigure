3fb7df6fdf561e15688dacebdf7e2390602e3089
Константин <300608kl@gmail.com>
2025-07-13 01:43:04 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python3
+sudo apt-get install python3-venv
+cd API_WebKlipperConfigure
+python3 -m venv venv
+chmod +x ./venv/bin/activate
+source ./venv/bin/activate
+pip install -r requrements.txt
+deactivate
