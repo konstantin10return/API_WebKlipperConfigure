8e1a15aec7a6093b9b900a25b17157ad5692d1f0
konstantin10return <300608kl@gmail.com>
2025-08-21 20:45:27 +0300

    Add files via upload

--- w_cfg.py
+++ w_cfg.py
@@ -1,10 +1,14 @@
+import os
+
 import api_get
 
 
 def get_path():
     with open("cfg_path.txt", 'r', encoding='utf-8') as file:
         content = file.read()
-    return content
+        if content[-1] == '\n':
+            content = content[:-1]
+    return os.path.expanduser(content)
 
 
 def write_cfg(content):
